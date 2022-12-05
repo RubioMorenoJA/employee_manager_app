41f3d3282445f063dfb75b28fbc5fed59feb0c70
Pope <j.rubio.piornal@gmail.com>
2022-12-05 16:20:49 +0100

    first commit on branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'employeemanagerapp';
+  public employees: Employee[] = [];
+  public selectedEmployee: Employee;
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+      this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 }
